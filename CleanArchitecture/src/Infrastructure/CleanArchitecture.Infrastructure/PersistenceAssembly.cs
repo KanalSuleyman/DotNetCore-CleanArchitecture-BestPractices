21425c1ee964dc85924bc522e0bbce1b1e5f4ca4
Suleyman Kanal <kanalSuleyman@outlook.com>
2025-02-01 02:47:35 +0300

    Create a marker structure for the persistence assembly

---
+++ CleanArchitecture/src/Infrastructure/CleanArchitecture.Infrastructure/PersistenceAssembly.cs
@@ -0,0 +1,7 @@
+﻿namespace CleanArchitecture.Persistence;
+
+/// <summary>
+/// Represents a marker structure for the repository assembly.
+/// This structure is used to identify and reference the assembly containing repository implementations.
+/// </summary>
+public struct PersistenceAssembly;
