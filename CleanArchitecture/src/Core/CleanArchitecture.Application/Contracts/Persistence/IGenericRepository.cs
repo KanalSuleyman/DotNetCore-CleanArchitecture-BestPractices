08c000f9e529690ce71b23498ba4164981715feb
Suleyman Kanal <kanalSuleyman@outlook.com>
2025-02-01 01:52:01 +0300

    Create abstract classes for repositories and unit of work pattern

---
+++ CleanArchitecture/src/Core/CleanArchitecture.Application/Contracts/Persistence/IGenericRepository.cs
@@ -0,0 +1,64 @@
+﻿using System.Linq.Expressions;
+
+namespace CleanArchitecture.Application.Contracts.Persistence;
+
+/// <summary>
+/// Defines a generic repository interface for performing CRUD operations on entities of type <typeparamref name="T"/>.
+/// </summary>
+/// <typeparam name="T">The type of entity managed by the repository.</typeparam>
+/// <typeparam name="TId">The type of the entity's identifier, which must be a value type.</typeparam>
+public interface IGenericRepository<T, in TId> where T : class where TId : struct
+{
+    /// <summary>
+    /// Retrieves all entities from the repository as a queryable collection.
+    /// </summary>
+    /// <returns>An <see cref="IQueryable{T}"/> representing the collection of entities.</returns>
+    IQueryable<T> GetAll();
+
+    Task<List<T>> GetAllAsync();
+
+    Task<List<T>> GetAllPagedAsync(int pageNumber, int pageSize);
+
+
+    /// <summary>
+    /// Checks asynchronously if an entity with the specified identifier exists in the repository.
+    /// </summary>
+    /// <param name="id">The identifier of the entity to check.</param>
+    /// <returns>A task representing the asynchronous operation. The task result is <c>true</c> if the entity exists; otherwise, <c>false</c>.</returns>
+    Task<bool> AnyAsync(TId id);
+
+    Task<bool> AnyAsync(Expression<Func<T, bool>> predicate);
+
+    /// <summary>
+    /// Filters entities based on the specified predicate and returns them as a queryable collection.
+    /// </summary>
+    /// <param name="predicate">A function to test each entity for a condition.</param>
+    /// <returns>An <see cref="IQueryable{T}"/> representing the filtered collection of entities.</returns>
+    IQueryable<T> Where(Expression<Func<T, bool>> predicate);
+
+    /// <summary>
+    /// Retrieves an entity by its identifier asynchronously.
+    /// </summary>
+    /// <param name="id">The identifier of the entity to retrieve.</param>
+    /// <returns>A task representing the asynchronous operation. The task result contains the entity if found; otherwise, <c>null</c>.</returns>
+    ValueTask<T?> GetByIdAsync(int id);
+
+    /// <summary>
+    /// Adds a new entity to the repository asynchronously.
+    /// </summary>
+    /// <param name="entity">The entity to add.</param>
+    /// <returns>A task representing the asynchronous operation.</returns>
+    ValueTask AddAsync(T entity);
+
+    /// <summary>
+    /// Updates an existing entity in the repository.
+    /// </summary>
+    /// <param name="entity">The entity to update.</param>
+    void Update(T entity);
+
+    /// <summary>
+    /// Deletes an entity from the repository.
+    /// </summary>
+    /// <param name="entity">The entity to delete.</param>
+    void Delete(T entity);
+}
---
+++ CleanArchitecture/src/Core/CleanArchitecture.Application/Contracts/Persistence/ISampleEntityCategoryRepository.cs
@@ -0,0 +1,23 @@
+﻿using CleanArchitecture.Domain.Entities;
+
+namespace CleanArchitecture.Application.Contracts.Persistence;
+
+/// <summary>
+/// Defines a repository interface for <see cref="SampleEntityCategory"/> entities, extending the generic repository.
+/// This interface includes additional methods specific to <see cref="SampleEntityCategory"/>.
+/// </summary>
+public interface ISampleEntityCategoryRepository : IGenericRepository<SampleEntityCategory, int>
+{
+    /// <summary>
+    /// Retrieves a <see cref="SampleEntityCategory"/> entity by its identifier, including its associated <see cref="id"/> entities.
+    /// </summary>
+    /// <param name="id">The identifier of the category to retrieve.</param>
+    /// <returns>A task representing the asynchronous operation. The task result contains the category if found; otherwise, <c>null</c>.</returns>
+    Task<SampleEntityCategory?> GetSampleEntityCategoryWithSampleEntitiesAsync(int id);
+
+    /// <summary>
+    /// Retrieves a queryable collection of <see cref="SampleEntityCategory"/> entities, including their associated <see cref="IQueryable{T}"/> entities.
+    /// </summary>
+    /// <returns>An <see cref="SampleEntity"/> representing the collection of categories with their associated entities.</returns>
+    Task<List<SampleEntityCategory?>> GetSampleEntityCategoryWithSampleEntities();
+}
