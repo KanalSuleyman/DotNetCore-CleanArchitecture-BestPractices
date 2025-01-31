e0a970a111f786da676d74b7cd8030607ae32858
Suleyman Kanal <kanalSuleyman@outlook.com>
2025-02-01 01:55:04 +0300

    Create a marker structure for service assembly

---
+++ CleanArchitecture/src/Core/CleanArchitecture.Application/ServiceAssembly.cs
@@ -0,0 +1,7 @@
+﻿namespace CleanArchitecture.Application;
+
+/// <summary>
+/// Represents a marker structure for the service assembly.
+/// This structure is used to identify and reference the assembly containing service implementations.
+/// </summary>
+public struct ServiceAssembly;
