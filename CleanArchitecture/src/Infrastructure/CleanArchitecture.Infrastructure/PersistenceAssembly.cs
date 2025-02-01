2441bd292f23b0fcf0d14c9e2892b68080bf9c96
Suleyman Kanal <kanalSuleyman@outlook.com>
2025-02-01 03:00:37 +0300

    Update summary comment

--- CleanArchitecture/src/Infrastructure/CleanArchitecture.Infrastructure/PersistenceAssembly.cs
+++ CleanArchitecture/src/Infrastructure/CleanArchitecture.Infrastructure/PersistenceAssembly.cs
@@ -1,7 +1,7 @@
 ﻿namespace CleanArchitecture.Persistence;
 
 /// <summary>
-/// Represents a marker structure for the repository assembly.
+/// Represents a marker structure for the persistence assembly.
 /// This structure is used to identify and reference the assembly containing repository implementations.
 /// </summary>
 public struct PersistenceAssembly;
